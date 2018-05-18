75660a3fb38d7d69ef14c605d6d272590cb907ab
Jeremy Winters <jeremyranierwinters@gmail.com>
2018-05-18 11:01:57 -0700

    added master

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -3,4 +3,4 @@ export SCRIPT_HOME=/tmp
 aws s3 cp $CONVERGDB_LIBRARY $SCRIPT_HOME/convergdb.zip
 aws s3 cp $CONVERGDB_SCRIPT  $SCRIPT_HOME/run.py
 
-spark-submit $SCRIPT_HOME/run.py
+spark-submit --master local[1] $SCRIPT_HOME/run.py
