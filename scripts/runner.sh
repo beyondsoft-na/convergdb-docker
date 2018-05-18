c0d3af1ba847c2173f9ec3d062e4566ea107060f
Jeremy Winters <jeremyranierwinters@gmail.com>
2018-05-18 11:54:58 -0700

    added env

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-env
 cat /etc/hosts
 export HOSTNAME=localhost
+env
+
 export SCRIPT_HOME=/tmp
 aws s3 cp $CONVERGDB_LIBRARY $SCRIPT_HOME/convergdb.zip
 aws s3 cp $CONVERGDB_SCRIPT  $SCRIPT_HOME/run.py
