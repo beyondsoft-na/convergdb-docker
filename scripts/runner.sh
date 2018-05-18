a66f9ca06b991d202234254441389f7939a93b72
Jeremy Winters <jeremyranierwinters@gmail.com>
2018-05-18 13:14:18 -0700

    added env

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -7,4 +7,4 @@ export SCRIPT_HOME=/tmp
 aws s3 cp $CONVERGDB_LIBRARY $SCRIPT_HOME/convergdb.zip
 aws s3 cp $CONVERGDB_SCRIPT  $SCRIPT_HOME/run.py
 
-spark-submit --master spark://localhost:7707 $SCRIPT_HOME/run.py
+spark-submit --master spark://127.0.0.1:7707 $SCRIPT_HOME/run.py
