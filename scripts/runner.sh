3f8be3dcf0f827a8e70b836ebd451adac4f02f27
Jeremy Winters <jeremyranierwinters@gmail.com>
2018-05-25 11:58:46 -0700

    testing for sts fix for s3 read credentials

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# temporary fix for hostname until ecs agent is updated
 echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
-cat /etc/hosts
-env
+
+export creds=`aws sts get-session-token`
+export AWS_ACCESS_KEY_ID=`echo $creds | jq -r ".Credentials.AccessKeyId"`
+export AWS_SECRET_ACCESS_KEY=`echo $creds | jq -r ".Credentials.SecretAccessKey"`
+export AWS_SESSION_TOKEN=`echo $creds | jq -r ".Credentials.SessionToken"`
 
 export SCRIPT_HOME=/tmp
 aws s3 cp $CONVERGDB_LIBRARY $SCRIPT_HOME/convergdb.zip
