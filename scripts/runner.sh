3c016d5fda3ee3a903e13c1a23b0097d18a9f836
Jeremy Winters <jeremyranierwinters@gmail.com>
2018-06-03 11:27:12 -0700

    added sts back

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -2,10 +2,10 @@
 # temporary fix for hostname until ecs agent is updated
 echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
 
-# export creds=`aws sts get-session-token`
-# export AWS_ACCESS_KEY_ID=`echo $creds | jq -r ".Credentials.AccessKeyId"`
-# export AWS_SECRET_ACCESS_KEY=`echo $creds | jq -r ".Credentials.SecretAccessKey"`
-# export AWS_SESSION_TOKEN=`echo $creds | jq -r ".Credentials.SessionToken"`
+export creds=`aws sts get-session-token`
+export AWS_ACCESS_KEY_ID=`echo $creds | jq -r ".Credentials.AccessKeyId"`
+export AWS_SECRET_ACCESS_KEY=`echo $creds | jq -r ".Credentials.SecretAccessKey"`
+export AWS_SESSION_TOKEN=`echo $creds | jq -r ".Credentials.SessionToken"`
 
 export SCRIPT_HOME=/tmp
 aws s3 cp $CONVERGDB_LIBRARY $SCRIPT_HOME/convergdb.zip
