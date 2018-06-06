eb602c402ff04463bc9a841cd3d1057fca8979cf
Jeremy Winters <jeremyranierwinters@gmail.com>
2018-06-06 12:26:59 -0700

    fixed token jq

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -8,13 +8,13 @@ if [ $? == 0 ]
   then
     export AWS_ACCESS_KEY_ID=`echo $creds | jq -r ".AccessKeyId"`
     export AWS_SECRET_ACCESS_KEY=`echo $creds | jq -r ".SecretAccessKey"`
-    export AWS_SESSION_TOKEN=`echo $creds | jq -r ".SessionToken"`;
+    export AWS_SESSION_TOKEN=`echo $creds | jq -r ".Token"`;
 fi
 
 env
 
 export SCRIPT_HOME=/tmp
-aws s3 cp --debug $CONVERGDB_LIBRARY $SCRIPT_HOME/convergdb.zip
-aws s3 cp --debug $CONVERGDB_SCRIPT  $SCRIPT_HOME/run.py
+aws s3 cp $CONVERGDB_LIBRARY $SCRIPT_HOME/convergdb.zip
+aws s3 cp $CONVERGDB_SCRIPT  $SCRIPT_HOME/run.py
 
 spark-submit --master spark://127.0.0.1:7707 $SCRIPT_HOME/run.py
