2bdbad06ec80a5347f3fcfff635533dffc794970
Jeremy Winters <jeremyranierwinters@gmail.com>
2018-06-06 11:00:52 -0700

    added debugs

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -11,8 +11,10 @@ if [ $? == 0 ]
     export AWS_SESSION_TOKEN=`echo $creds | jq -r ".SessionToken"`;
 fi
 
+env
+
 export SCRIPT_HOME=/tmp
-aws s3 cp $CONVERGDB_LIBRARY $SCRIPT_HOME/convergdb.zip
-aws s3 cp $CONVERGDB_SCRIPT  $SCRIPT_HOME/run.py
+aws s3 cp --debug $CONVERGDB_LIBRARY $SCRIPT_HOME/convergdb.zip
+aws s3 cp --debug $CONVERGDB_SCRIPT  $SCRIPT_HOME/run.py
 
 spark-submit --master spark://127.0.0.1:7707 $SCRIPT_HOME/run.py
