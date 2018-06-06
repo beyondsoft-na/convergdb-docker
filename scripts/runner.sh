e1a1b8a0a37cd558321b95ca1d0d380c143effa9
Jeremy Winters <jeremyranierwinters@gmail.com>
2018-06-05 23:18:10 -0700

    introspection approach

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -2,10 +2,14 @@
 # temporary fix for hostname until ecs agent is updated
 echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
 
-export creds=`aws sts get-session-token`
-export AWS_ACCESS_KEY_ID=`echo $creds | jq -r ".Credentials.AccessKeyId"`
-export AWS_SECRET_ACCESS_KEY=`echo $creds | jq -r ".Credentials.SecretAccessKey"`
-export AWS_SESSION_TOKEN=`echo $creds | jq -r ".Credentials.SessionToken"`
+export creds=`curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI`
+
+if [ $? == 0 ]
+  then
+    export AWS_ACCESS_KEY_ID=`echo $creds | jq -r ".AccessKeyId"`
+    export AWS_SECRET_ACCESS_KEY=`echo $creds | jq -r ".SecretAccessKey"`
+    export AWS_SESSION_TOKEN=`echo $creds | jq -r ".SessionToken"`;
+fi
 
 export SCRIPT_HOME=/tmp
 aws s3 cp $CONVERGDB_LIBRARY $SCRIPT_HOME/convergdb.zip
